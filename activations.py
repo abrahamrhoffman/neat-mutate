8385c77d34900175c49741eb7a83088f86c2d6a4
Abraham Hoffman <abrahamrhoffman@gmail.com>
2017-04-13 16:28:40 -0700

    activations.py update

--- activations.py
+++ activations.py
@@ -10,71 +10,71 @@ from __future__ import division
 import math
 
 ### Activation Class ###
-class Activation(object):
+class Activations(object):
 
-    def sigmoid_activation(self, z):
+    def sigmoid(self, z):
         z = max(-60.0, min(60.0, 5.0 * z))
         return 1.0 / (1.0 + math.exp(-z))
 
-    def tanh_activation(self, z):
+    def tanh(self, z):
         z = max(-60.0, min(60.0, 2.5 * z))
         return math.tanh(z)
 
-    def sin_activation(self, z):
+    def sin(self, z):
         z = max(-60.0, min(60.0, 5.0 * z))
         return math.sin(z)
 
-    def gauss_activation(self, z):
+    def gauss(self, z):
         z = max(-3.4, min(3.4, z))
         return math.exp(-5.0 * z**2)
 
-    def relu_activation(self, z):
+    def relu(self, z):
         return z if z > 0.0 else 0.0
 
-    def softplus_activation(self, z):
+    def softplus(self, z):
         z = max(-60.0, min(60.0, 5.0 * z))
         return 0.2 * math.log(1 + math.exp(z))
 
-    def identity_activation(self, z):
+    def identity(self, z):
         return z
 
-    def clamped_activation(self, z):
+    def clamped(self, z):
         return max(-1.0, min(1.0, z))
 
-    def inv_activation(self, z):
+    def inv(self, z):
         if z == 0:
             return 0.0
         else:
             return 1.0 / z
 
-    def log_activation(self, z):
+    def log(self, z):
         z = max(1e-7, z)
         return math.log(z)
 
-    def exp_activation(self, z):
+    def exp(self, z):
         z = max(-60.0, min(60.0, z))
         return math.exp(z)
 
-    def abs_activation(self, z):
+    def abs(self, z):
         return abs(z)
 
-    def hat_activation(self, z):
+    def hat(self, z):
         return max(0.0, 1 - abs(z))
 
-    def square_activation(self, z):
+    def square(self, z):
         return z ** 2
 
-    def cube_activation(self, z):
+    def cube(self, z):
         return z ** 3
 
-    def abs_activation(z):
+    def abs(z):
         return abs(z)
 
-    def hat_activation(z):
+    def hat(z):
         return max(0.0, 1 - abs(z))
 
-    def square_activation(z):
+    def square(z):
         return z ** 2
 
-    def cube_activation(z):
+    def cube(z):
         return z ** 3
