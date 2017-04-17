261dfabb29dbea31b38be72a57dee2c18794fb80
Abraham Hoffman <abrahamrhoffman@gmail.com>
2017-04-17 11:49:23 -0700

    multigpu testing

---
+++ neural_network.py
@@ -0,0 +1,18 @@
+#######################
+# Mutate: Activations #
+#######################
+# Author: Abe Hoffman #
+# Date  : Apr 2017    #
+#######################
+
+### Import Libraries ###
+from __future__ import print_function
+
+### Neural Network Class ###
+class Neural_Network(object):
+
+    def epochs(self, z):
+        return z
+
+    def layer0(self, z):
+        return z
