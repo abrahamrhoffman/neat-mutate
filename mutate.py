aa03aee05fb7d3481d0844d316720eeb6a5039ff
Abraham Hoffman <abrahamrhoffman@gmail.com>
2017-05-07 01:04:23 -0700

    sync

--- mutate.py
+++ mutate.py
@@ -1,9 +1,9 @@
 from __future__ import print_function
-import os;os.environ['TF_CPP_MIN_LOG_LEVEL']='3'
-import sys
+import sys,os;os.environ['TF_CPP_MIN_LOG_LEVEL']='3'
 from genome import Genome
 from phenome import Phenome
 from fitness import Fitness
+from report import Report
 
 class NEAT(object):
     '''
@@ -14,19 +14,11 @@ class NEAT(object):
         self.data = data
 
     def run(self):
-        g = Genome(self.data)           # Instantiate the Genome Class with our XOR Data
+        g = Genome(self.data)           # Instantiate the Genome Class
         GENOME = g.create()             # Create an indirectly encoded (NEAT) Genome
-        p = Phenome(GENOME)             # Instantiate the Phenome Class with our newly minted Genome
+        p = Phenome(GENOME)             # Instantiate the Phenome Class with Genome
         PHENOME = p.create()            # Create a Phenome (Neural Network)
-        f = Fitness(self.data,PHENOME)  # Evaluate Phenome Fitness
-        FITNESS = f.evaluate()
-
-        X,Y = self.data
-        result,error,solved = FITNESS
-
-        print('Expected:')
-        print('{}'.format(Y))
-        print('Result:')
-        print('{}'.format(result))
-        print('Error:')
-        print('{}'.format(error))
+        f = Fitness(self.data,PHENOME)  # Instantiate Phenome Fitness (Tensorflow)
+        FITNESS = f.evaluate()          # Evaluate Phenome Fitness
+        r = Report(FITNESS)             # Instantiate the Report Class
+        REPORT = r.start()              # Print the Report to StdOut
