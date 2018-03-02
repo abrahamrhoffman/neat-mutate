438e1ec6af25948755e504f225a5acb7d4d2b31e
Abraham Hoffman <abrahamrhoffman@gmail.com>
2018-03-02 02:17:26 -0700

    Update visualize.py

--- visualize.py
+++ visualize.py
@@ -13,9 +13,11 @@ class Visualize(object):
         outputCluster = [row['node'] for ix,row in outputDF[['node', 'out']].iterrows()]
 
         notOutputDF = onlyEnabledDF.loc[onlyEnabledDF['type'] != "output"]
-        hiddenCluster = [row['node'] for ix,row in notOutputDF[['node', 'out']].iterrows()]
+        notSensorDF = notOutputDF.loc[notOutputDF['type'] != "sensor"]
+        hiddenCluster = [row['node'] for ix,row in notSensorDF[['node', 'out']].iterrows()]
 
-        sensorDF = notOutputDF.loc[notOutputDF['type'] == "sensor"]
+        notHiddenDF = notOutputDF.loc[notOutputDF['type'] != "hidden"]
+        sensorDF = notHiddenDF.loc[notHiddenDF['type'] == "sensor"]
         sensorCluster = [row['node'] for ix,row in sensorDF[['node', 'out']].iterrows()]
 
         graphDF = notOutputDF[['node', 'out']]
@@ -28,7 +30,8 @@ class Visualize(object):
         G = pgv.AGraph(graphString, strict=False, directed=True, rankdir='LR')
         G.node_attr['shape']='circle'
         G.add_subgraph(sensorCluster, name='cluster_sensors', label="Sensor Nodes", rank="same")
-        G.add_subgraph(hiddenCluster, name='cluster_hidden', label="Hidden Nodes")#, rank="same")
+        G.add_subgraph(hiddenCluster, name='cluster_hidden', label="Hidden Nodes")
         G.add_subgraph(outputCluster, name='cluster_output', label="Output Nodes", rank="same")
 
-        return Image(G.draw(format='png', prog='dot'))
+        imageResult = G.draw(format='png', prog='dot')
+        return imageResult
