7a60561e63ab17a0cb88cb061d815bb533ae9dec
Abraham Hoffman <abrahamrhoffman@gmail.com>
2018-03-09 11:52:48 -0700

    Update visualize.py

--- visualize.py
+++ visualize.py
@@ -6,32 +6,48 @@ class Visualize(object):
     Mutate Visualization: Using PyGraphViz and Dot Syntax
     '''
 
-    def makeGraph(self, aDataFrameToGraph):
+    def makeGraph(aDataFrameToGraph):
         onlyEnabledDF = aDataFrameToGraph.loc[aDataFrameToGraph['enabled'] == True]
 
+        # Segregate the Sensor Nodes
+        sensorDF = onlyEnabledDF.loc[onlyEnabledDF['type'] == "sensor"]
+        # Create a list of sensor node connections
+        sensorConnections = sensorDF[['node', 'out']].values.tolist()
+        # Create a list of sensor nodes to cluster
+        sensorNodes = sensorDF['node'].values.tolist()
+
+        # Segregate the Hidden Nodes
+        hiddenDF = onlyEnabledDF.loc[onlyEnabledDF['type'] == "hidden"]
+        # Create a list of hidden node connections
+        hiddenConnections = hiddenDF[['node', 'out']].values.tolist()
+        # Create a list of hidden nodes to cluster
+        hiddenNodes = hiddenDF['node'].values.tolist()
+
+        # Segregate the Output Nodes
         outputDF = onlyEnabledDF.loc[onlyEnabledDF['type'] == "output"]
-        outputCluster = [row['node'] for ix,row in outputDF[['node', 'out']].iterrows()]
+        # Create a list of output node connections
+        outputConnections = outputDF[['node', 'out']].values.tolist()
+        # Create a list of output nodes to cluster
+        outputNodes = outputDF['node'].values.tolist()
 
-        notOutputDF = onlyEnabledDF.loc[onlyEnabledDF['type'] != "output"]
-        notSensorDF = notOutputDF.loc[notOutputDF['type'] != "sensor"]
-        hiddenCluster = [row['node'] for ix,row in notSensorDF[['node', 'out']].iterrows()]
+        # Create the List of ClusterLists
+        # Do not include the Output cluster as it would create invalid recursive edges
+        aLargeList = []
+        aLargeList.extend(sensorConnections)
+        aLargeList.extend(hiddenConnections)
 
-        notHiddenDF = notOutputDF.loc[notOutputDF['type'] != "hidden"]
-        sensorDF = notHiddenDF.loc[notHiddenDF['type'] == "sensor"]
-        sensorCluster = [row['node'] for ix,row in sensorDF[['node', 'out']].iterrows()]
-
-        graphDF = notOutputDF[['node', 'out']]
-
+        # Create the dot syntax map
         graphString = ("digraph {")
-        for ix,row in graphDF.iterrows():
-            graphString += (str(row['node']) + "->" + str(row['out']) + ";")
+        for ix,ele in enumerate(aLargeList):
+            graphString += (str(ele[0]) + "->" + str(ele[1]) + ";")
         graphString += ("}")
 
+        # Create the Graph
         G = pgv.AGraph(graphString, strict=False, directed=True, rankdir='LR')
         G.node_attr['shape']='circle'
-        G.add_subgraph(sensorCluster, name='cluster_sensors', label="Sensor Nodes", rank="same")
-        G.add_subgraph(hiddenCluster, name='cluster_hidden', label="Hidden Nodes")
-        G.add_subgraph(outputCluster, name='cluster_output', label="Output Nodes", rank="same")
+        G.add_subgraph(sensorNodes, name='cluster_sensors', label="Sensor Nodes", rank="same")
+        G.add_subgraph(hiddenNodes, name='cluster_hidden', label="Hidden Nodes")
+        G.add_subgraph(outputNodes, name='cluster_output', label="Output Nodes", rank="same")
 
         imageResult = G.draw(format='png', prog='dot')
         return imageResult
