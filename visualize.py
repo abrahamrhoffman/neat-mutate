6411c4a3fe50f328e2aa6efab86679a570300c04
Abraham Hoffman <abrahamrhoffman@gmail.com>
2018-03-09 12:30:25 -0700

    All nodes and edges are properly created now (bug) fixed

--- visualize.py
+++ visualize.py
@@ -8,37 +8,37 @@ class Visualize(object):
 
     def makeGraph(self, aDataFrameToGraph):
         onlyEnabledDF = aDataFrameToGraph.loc[aDataFrameToGraph['enabled'] == True]
+        removeOutputDF = onlyEnabledDF.loc[onlyEnabledDF['type'] != ("output")]
+
+        # Node and Edge Logic
+        connectionList = []
+        for ix,row in removeOutputDF[['node','in','out']].iterrows():
+            # If it is a sensor
+            if row['node'] == row['in']:
+                connectionList.append(row[['in', 'out']].values.tolist())
+            # If it isn't a sensor, ensure both edges are created
+            else:
+                connectionList.append(row[['in', 'node']].values.tolist())
+                connectionList.append(row[['node', 'out']].values.tolist())
 
         # Segregate the Sensor Nodes
         sensorDF = onlyEnabledDF.loc[onlyEnabledDF['type'] == "sensor"]
-        # Create a list of sensor node connections
-        sensorConnections = sensorDF[['node', 'out']].values.tolist()
         # Create a list of sensor nodes to cluster
         sensorNodes = sensorDF['node'].values.tolist()
 
         # Segregate the Hidden Nodes
         hiddenDF = onlyEnabledDF.loc[onlyEnabledDF['type'] == "hidden"]
-        # Create a list of hidden node connections
-        hiddenConnections = hiddenDF[['node', 'out']].values.tolist()
         # Create a list of hidden nodes to cluster
         hiddenNodes = hiddenDF['node'].values.tolist()
 
         # Segregate the Output Nodes
         outputDF = onlyEnabledDF.loc[onlyEnabledDF['type'] == "output"]
-        # Create a list of output node connections
-        outputConnections = outputDF[['node', 'out']].values.tolist()
         # Create a list of output nodes to cluster
         outputNodes = outputDF['node'].values.tolist()
 
-        # Create the List of ClusterLists
-        # Do not include the Output cluster as it would create invalid recursive edges
-        aLargeList = []
-        aLargeList.extend(sensorConnections)
-        aLargeList.extend(hiddenConnections)
-
         # Create the dot syntax map
         graphString = ("digraph {")
-        for ix,ele in enumerate(aLargeList):
+        for ix,ele in enumerate(connectionList):
             graphString += (str(ele[0]) + "->" + str(ele[1]) + ";")
         graphString += ("}")
 
@@ -48,6 +48,6 @@ class Visualize(object):
         G.add_subgraph(sensorNodes, name='cluster_sensors', label="Sensor Nodes", rank="same")
         G.add_subgraph(hiddenNodes, name='cluster_hidden', label="Hidden Nodes")
         G.add_subgraph(outputNodes, name='cluster_output', label="Output Nodes", rank="same")
-
         imageResult = G.draw(format='png', prog='dot')
+
         return imageResult
