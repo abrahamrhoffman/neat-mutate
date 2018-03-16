b7be5160a2f986465a0b76d04b50dda683a34a55
Abraham Hoffman <abrahamrhoffman@gmail.com>
2018-03-16 01:55:32 -0700

    Multi-layer NN Weights
    Need to fix SettingWithCopyWarning slicing in dataframe

--- phenome.py
+++ phenome.py
@@ -11,7 +11,8 @@ class Phenome(object):
     '''
 
     def __init__(self, GENOME):
-        self.genome = GENOME
+        # Only use enabled genes
+        self.genome = GENOME.loc[GENOME['enabled'] == True]
 
     def create(self):
         '''
@@ -19,29 +20,15 @@ class Phenome(object):
         2. Return a list of all layer weights in Torch Tensor format
         '''
 
-        # Prepare partial dataframes for weight and order selection
-        onlyEnabledDF = self.genome.loc[self.genome['enabled'] == True]
-        removeSensorDF = onlyEnabledDF.loc[onlyEnabledDF['type'] != ("sensor")]
-        removeOutputDF = onlyEnabledDF.loc[onlyEnabledDF['type'] != ("output")]
-        weightsDF = removeOutputDF.loc[removeOutputDF['type'] != ("sensor")]
-
-        # Store all target nodes (non-sensor) in list
-        nodeWeightList = removeSensorDF['node'].values.tolist()
-
-        # Build the list of nodes and their incoming weights
-        aWeightList = []
-        for ele in nodeWeightList:
-            A = weightsDF.loc[weightsDF['node'] == ele]
-            B = weightsDF.loc[weightsDF['out'] == ele]
-            C = pd.concat([A,B]).drop_duplicates().reset_index(drop=True)
-            aWeightList.append([ele,C['weight'].values.tolist()])
+        # Only use enabled genes
+        #self.genome = self.genome.loc[self.genome['enabled'] == True]
 
         # Build the Neural Network layers
-        sensorNodes = sorted(onlyEnabledDF.loc[onlyEnabledDF['type'] == ("sensor")]['node'].values.tolist())
-        outputNodes = sorted(onlyEnabledDF.loc[onlyEnabledDF['type'] == ("output")]['node'].values.tolist())
+        sensorNodes = sorted(self.genome.loc[self.genome['type'] == ("sensor")]['node'].values.tolist())
+        outputNodes = sorted(self.genome.loc[self.genome['type'] == ("output")]['node'].values.tolist())
 
         # Add the DataFrame Column
-        self.genome['layer'] = ""
+        self.genome.loc[:,'layer'] = ("")
 
         # Assign sensor nodes to layer 0
         self.genome.loc[self.genome.type == "sensor", "layer"] = ("0")
@@ -66,17 +53,15 @@ class Phenome(object):
             if row['in'] in outputNodes:
                 self.genome.loc[ix, "layer"] = str(int(lastLayerNum)+1)
 
-        return self.genome
-        # Select the sensor genes
-        #sensors = enabledDF.loc[enabledDF['type'] == ('sensor')]
-        # Select the sensor weight values
-        #sensor_weights = sensors['weight'].values
-        # Reshape the Tensor for MatMul
-        #sensor_weights = sensor_weights.reshape([(sensor_weights.shape[0]),1])
-        # Convert to Torch Tensor
-        #sensor_weights = torch.from_numpy(sensor_weights)
+        # Build a weight list based on the layers
+        layers = sorted(self.genome['layer'].unique())
+        weightList = []
+        for layer in layers:
+            weightList.append([self.genome.loc[self.genome['layer'] == (layer)]['weight'].values])
+        # Reshape Numpy Arrays as non-zero-dim
+        weightList = [ele[0].reshape([(ele[0].shape[0]),1]) for ele in weightList]
+        # Convert to Torch Tensors
+        weightList = [torch.from_numpy(ele) for ele in weightList]
 
         ## Return all layer weights ##
-        #PHENOME = []
-        #PHENOME.append(sensor_weights)
-        #return PHENOME
+        return weightList
