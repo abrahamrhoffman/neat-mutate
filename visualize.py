1666bf730d985f180b5591ad7e2b94fafc7f8308
Abraham Hoffman <abrahamrhoffman@gmail.com>
2018-04-18 11:21:02 -0700

    Simple and Advanced node clustering

--- visualize.py
+++ visualize.py
@@ -6,21 +6,18 @@ class Visualize(object):
     Mutate Visualization: Using PyGraphViz and Dot Syntax
     '''
 
-    def __init__(self, GENOME):
+    def __init__(self, GENOME,simple=True):
         self.nodes, self.connections = GENOME
+        self.simple = simple
 
     def create(self):
         '''
         Create a directed graph from a Genome (Genotype)
         '''
-        
-        # Capture a List of Lists for in/out connections
-        connectionList = self.connections[['in','out']].values.tolist()
 
-        # Cluster nodes by type
-        sensorNodes = self.nodes.loc[self.nodes.type == ("sensor"),("node")].values.tolist()
-        hiddenNodes = self.nodes.loc[self.nodes.type == ("hidden"),("node")].values.tolist()
-        outputNodes = self.nodes.loc[self.nodes.type == ("output"),("node")].values.tolist()
+        # Capture a List of Lists for in/out connections
+        enabled = self.connections.loc[self.connections.enabled == True]
+        connectionList = enabled[['in','out']].values.tolist()
 
         # Create the dot syntax map
         graphString = ("digraph {")
@@ -31,30 +28,43 @@ class Visualize(object):
         # Create the Graph
         G = pgv.AGraph(graphString, strict=True, directed=True, rankdir='LR')
         G.node_attr['shape']='circle'
-        #G.add_subgraph(sensorNodes, name='cluster_sensors', label="Sensor Nodes", rank="same")
-        #G.add_subgraph(hiddenNodes, name='cluster_hidden', label="Hidden Nodes")
-        #G.add_subgraph(outputNodes, name='cluster_output', label="Output Nodes", rank="same")
 
-        # Create a list of all the layers in the nodes DataFrame
-        layerList = sorted(self.nodes.layer.unique().tolist())
-        # Use the list to create a subgraph for each cluster of nodes in each layer
-        for ix,ele in enumerate(layerList):
-            # Select the nodes in layer ele as a list
-            layerN = self.nodes.loc[self.nodes.layer == ele,("node")].values.tolist()
-            # Create a name for a new subgraph
-            aName = ("cluster_" + str(ele))
-            # Create a label for a new subgraph
-            aLabel = ("layer " + str(ele))
-            # Add the subgraph to the Graph
-            if layerList[-1] == ix:
-                G.add_subgraph(layerN, name=aName, label=("output"), rank="same")
-            elif layerList[0] == ix:
-                G.add_subgraph(layerN, name=aName, label=("sensor"), rank="same")
-            else:
-                G.add_subgraph(layerN, name=aName, label=aLabel, rank="same")
+        ## Simple grouping
+        # This grouping layout demonstrates a basic clustering of nodes and edges
+        # Note: The Phenome class executes per the 'Advanced Grouping' layout
+        #       For debugging, use simple=False
+        if self.simple:
+            sensorNodes = self.nodes.loc[self.nodes.type == ("sensor"),("node")].values.tolist()
+            hiddenNodes = self.nodes.loc[self.nodes.type == ("hidden"),("node")].values.tolist()
+            outputNodes = self.nodes.loc[self.nodes.type == ("output"),("node")].values.tolist()
         
+            G.add_subgraph(sensorNodes, name='cluster_sensors', label="Sensor Nodes", rank="same")
+            G.add_subgraph(hiddenNodes, name='cluster_hidden', label="Hidden Nodes")
+            G.add_subgraph(outputNodes, name='cluster_output', label="Output Nodes", rank="same")
+        
+        ## Advanced Grouping
+        # This grouping layout demonstrates how the Phenome will arrange weights in the Neural Network
+        if not self.simple:
+            # Create a list of all the layers in the nodes DataFrame
+            layerList = sorted(self.nodes.layer.unique().tolist())
+            # Use the list to create a subgraph for each cluster of nodes in each layer
+            for ix,ele in enumerate(layerList):
+                # Select the nodes in layer ele as a list
+                layerN = self.nodes.loc[self.nodes.layer == ele,("node")].values.tolist()
+                # Create a name for a new subgraph
+                aName = ("cluster_" + str(ele))
+                # Create a label for a new subgraph
+                aLabel = ("layer " + str(ele))
+                # Add the subgraph to the Graph
+                if layerList[-1] == ix:
+                    G.add_subgraph(layerN, name=aName, label=("output"), rank="same")
+                elif layerList[0] == ix:
+                    G.add_subgraph(layerN, name=aName, label=("sensor"), rank="same")
+                else:
+                    G.add_subgraph(layerN, name=aName, label=aLabel, rank="same")
+
         G.graph_attr.update(dpi="90")
-        
+
         imageResult = G.draw(format='png', prog='dot')
 
         return imageResult
